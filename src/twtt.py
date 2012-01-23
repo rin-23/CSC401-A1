5569fa06617665fa6eb50e8f1fcf1cba8a598e3e
Rinat Abdrashitov <rindopuz23@gmail.com>
2012-01-22 20:31:50 -0500

    Part 1 Draft 2

--- src/twtt.py
+++ src/twtt.py
@@ -3,24 +3,21 @@ from NLPlib import *
 import re
 
 if __name__ == "__main__":
-    input_fname = sys.argv[1]
-    output_fname = sys.argv[2]
-    input_fpntr = open(input_fname, "r")
-    output_fpntr = open(output_fname, "w")
+    input_fpntr = open(sys.argv[1], "r")
+    output_fpntr = open(sys.argv[2], "w")
     clitics = ["'m", "'re", "'s", "'ll", "'ve", "n't"]
     
     tagger = NLPlib()
-    print tagger.tag(['/'])
     for line in input_fpntr:
-        output_fpntr.write('|')
+        output_fpntr.write('|\n')
+        output_fpntr.write("ORIGINAL: " + line)
+        
+        line = re.sub(r'&quot|&amp', '', line) #get rid of quot and amp
+        line = re.sub(r'\. *?\. *?\.', ' &ellipsis ', line) # substitute ... with &elipsis to avoid multiple periods
+        line = re.sub(r'@?<(.*) .*?>.*?</\1>', '', line) #remove html tags
+        line = re.sub(r'(?:http://|ftp://|www\.)\S+\.\S+', '', line) #remove urls
 
-        origin = line
-        line = re.sub(r'&quot|&amp', '', line)
-        line = re.sub(r'\. *?\. *?\.', ' &ellipsis ', line)
-        nolinks = re.sub(r'<(.*) .*?>.*?</\1>', '', line) #remove html tags
-        nourls = re.sub(r'(?:http://|ftp://|www\.)\S+\.\S+', '', nolinks) #remove urls
-
-        newlinearray = re.findall('([^.]*?[.!?\n]+)', nourls) #separate sentences in the tweet
+        newlinearray = re.findall('([^.]*?[.!?\n]+)', line) #separate sentences in the tweet
 
         tokens = [re.split(r" +", line.strip()) for line in newlinearray] # separate every word using space as separator
 
@@ -29,75 +26,26 @@ if __name__ == "__main__":
         for sentence in tokens:
             newsentence = []
             for word in sentence:
-                newtokens = re.split("('(?:m|re|s|ll|ve|t)|n't|&ellipsis|[^\w\s])", word)
+                newtokens = re.compile("('(?:m|re|s|ll|ve|t)|n't|&ellipsis|[^\w\s])", re.IGNORECASE).split(word)
                 noempty = [re.sub(r'&ellipsis', '...', newtoken.strip()) for newtoken in newtokens if newtoken.strip() != '']
                 newsentence = newsentence + noempty
             if (newsentence != []): nopunctiation.append(newsentence)
 
-
-
-        print "|"
-        print origin
-        #print tokens
-        #print nopunctiation
         sent = nopunctiation
         tags = [tagger.tag(sent) for sent in nopunctiation ]
-        #print tags
 
         zipper = lambda x, y, z: [x[i]+y[i]+z[i] for i in range(0, len(x))]
         zipped = [zipper(nopunctiation[i], ['/']*len(tags[i]), tags[i]) for i in range(0, len(tags))]
-        #print zipped
 
         for sentence in zipped:
             for i in range (0, len(sentence)):
-                sys.stdout.write(sentence[i])
-                if i != len(sentence)-1: sys.stdout.write(' ')
-            sys.stdout.write('\n')
+                output_fpntr.write(sentence[i])
+                if i != len(sentence) - 1: output_fpntr.write(' ')
+            output_fpntr.write('\n')
                 
+        #output_fpntr.write('|\n')
 
-
-        print '|'
-        #output_fpntr.write(line)  
-        #output_fpntr.write('|')
-
-#       noclitics = []
-#        for line in newlinearray:
-#            prog = re.compile(.*?, flags)            for clitic in clitics:
-#                line = line.replace(clitic, " " + clitic)
-#            noclitics.append(line)
-
-
-
-#        newlinearray = [temparray[i] for i in range (1, len(temparray), 2)] #array of sentences in the tweet
-
-#        noclitics = []
-#        for newline in newlinearray:
-#            srch = re.search(r"(?P<sub>'(?:m|re|s|ll|ve|t)|n't|[^\w\s])", newline)
-#            if srch != None:
-#                new = re.sub(srch.group("sub"), " " + srch.group("sub"), newline)
-#                noclitics.append(new)
-#            else:
-#                noclitics.append(newline)
-
-#
-#        tokens = [re.split(r" +", line.strip()) for line in newlinearray]
-#        tokens2 = []
-#        for sentence in tokens:
-#            newsentence = []
-#            for word in sentence:
-#                newwords = re.split("('(?:m|re|s|ll|ve|t)|n't)", word)
-#                for newword in newwords:
-#                    if  not (newword.strip() in clitics):
-#                        newword = re.split("([^\w\s])", newword.strip())
-#                        newsentence = newsentence + newword
-#                    else:
-#                        newsentence = newsentence.append(newword)
-#            tokens2.append(newsentence)
-##
-#        tokens3 = []
-
-        #tokens2 = [re.split(r"") for token in tokens]
-
-
+    #close file pointers
     input_fpntr.close()
-    output_fpntr.close()   
+    output_fpntr.close()
+    print("Done")
