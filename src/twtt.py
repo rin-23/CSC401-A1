1788a05d71644c93fc33c18227c9f24f8ee88d83
Rinat Abdrashitov <rindopuz23@gmail.com>
2012-02-03 04:24:34 -0500

    update

--- src/twtt.py
+++ src/twtt.py
@@ -2,8 +2,35 @@ import sys
 from NLPlib import *
 import re
 
-#!!!!!!!!!Also consider tags as <a/>
 
+#TODO Questions
+#Handle multiple dashes
+#How many dots in the ellipses
+
+#TODO Extra things
+#Add smiles support
+#add special html characters support
+
+def remove_html_url(line):
+     line = re.sub("@<[^<>]+?>\S+?</[^<>]+?>", '', line) #remove @username
+     line = re.sub("<[^<>]+?>#\S+?</[^<>]+?>", '', line) #remove hashtags
+     line = re.sub("<[^<>]+?>", '', line) #remove html tags
+     line = re.sub("(?:http://|ftp://|www\.)\S+\.\S+", '', line) #remove urls
+     return line
+
+def remove_html_special_char(line):
+    #substitute &quot;, &amp;, &lt; &gt; with appropriate symbol
+    line = re.sub("&quot;", '"', line)
+    line = re.sub("&amp;", '&', line)
+    line = re.sub("&lt;", '<', line)
+    line = re.sub("&gt;", '>', line)
+    return line
+
+#TODO find a better name
+def sub_html_special_char(newtoken):
+    newtoken = re.sub("&hellip;", '...', newtoken)
+    newtoken = re.sub("&mdash;", "--", newtoken)
+    return newtoken
 
 
 if __name__ == "__main__":
@@ -16,20 +43,14 @@ if __name__ == "__main__":
         output_fpntr.write('ORG:' + line + '\n') #TODO: delete
         
         # substitute ...(ellipsis) with &hellip; to avoid multiple periods
-        line = re.sub("\.\.\.", " &hellip; ", line)  
-        line = re.sub('@<.+?>\S+?</.+?>', '', line) #remove @username
-        line = re.sub("<.+?>#\S+?</.+?>", '', line) #remove hashtags
-        line = re.sub("<.+?>", '', line) #remove html tags
-        line = re.sub("(?:http://|ftp://|www\.)\S+\.\S+", '', line) #remove urls
+        line = re.sub("\.[ \t]?\.[ \t]?\.[ \t]?", " &hellip; ", line)
+        #substitute em dash multiple dashes
+        line = re.sub("--", " &mdash; ", line)
 
-        #substitute &quot;, &amp;, &lt; &gt; with appropriate symbol
-        line = re.sub("&quot;", '"', line)
-        line = re.sub("&amp;", '&', line)
-        line = re.sub("&lt;", '<', line)
-        line = re.sub("&gt;", '>', line)
+        line = remove_html_url(line)
+        line = remove_html_special_char(line)
 
-        #DONT FORGET TO ADD SUPPORT FOR " after end of sentece!!!
-        newlinearray = re.findall("([^.]*?[.!?\n]+)", line) #separate sentences in the tweet
+        newlinearray = re.findall("[^.?!]+[.!?\r\n]+(?:(?:\s+[.!?\r\n]+)|\s*\")*", line) #separate sentences in the tweet
 
         tokens = [re.split(" +", line.strip()) for line in newlinearray] # separate every word using space as separator
 
@@ -38,8 +59,8 @@ if __name__ == "__main__":
         for sentence in tokens:
             newsentence = []
             for word in sentence:
-                newtokens = re.compile("('(?:m|re|s|ll|ve|t)|n't|&hellip;|[^\w\s])", re.IGNORECASE).split(word)
-                noempty = [re.sub("&hellip;", '...', newtoken.strip()) for newtoken in newtokens if newtoken.strip() != '']
+                newtokens = re.compile("('(?:m|re|s|ll|ve|t)|n't|&hellip;|&mdash;|[^\w\s])", re.IGNORECASE).split(word)
+                noempty = [sub_html_special_char(newtoken.strip()) for newtoken in newtokens if newtoken.strip() != '']
                 newsentence = newsentence + noempty
             if (newsentence != []): nopunctiation.append(newsentence)
 
